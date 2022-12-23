6a11bb00688b878214955164f36aaca3a8fb06eb
User <User@LAPTOP-VPU69J4J>
2022-12-23 19:33:25 +0800

    加入專案檔案。

---
+++ 找零程式設計/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace 找零程式設計
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPurchase = new System.Windows.Forms.TextBox();
+            this.txtPayment = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPurchase
+            // 
+            this.txtPurchase.Location = new System.Drawing.Point(89, 12);
+            this.txtPurchase.Name = "txtPurchase";
+            this.txtPurchase.Size = new System.Drawing.Size(191, 25);
+            this.txtPurchase.TabIndex = 0;
+            this.txtPurchase.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPayment
+            // 
+            this.txtPayment.Location = new System.Drawing.Point(89, 43);
+            this.txtPayment.Name = "txtPayment";
+            this.txtPayment.Size = new System.Drawing.Size(191, 25);
+            this.txtPayment.TabIndex = 1;
+            this.txtPayment.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(182, 74);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(98, 45);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "開始找零";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(12, 146);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 15);
+            this.labelResult.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "購買金額:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "支付金額:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 673);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtPayment);
+            this.Controls.Add(this.txtPurchase);
+            this.Controls.Add(this.labelResult);
+            this.Name = "Form1";
+            this.Text = "找零程式";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPurchase;
+        private System.Windows.Forms.TextBox txtPayment;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 找零程式設計/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 找零程式設計
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            int[] m = new int[6] { 500, 100, 50, 10, 5, 1 };
+            int[] r = new int[6];
+
+            int purchase = Convert.ToInt16( txtPurchase.Text );
+            int payment = Convert.ToInt16( txtPayment.Text );
+
+            payment = payment - purchase;
+
+            for( int i=0; i<m.Length; i++ )
+            {
+                r[i] = payment / m[i];
+                payment = payment % m[i];
+            }
+
+            labelResult.Text = "";
+
+            for (int i = 0; i < m.Length; i++)
+            {
+                labelResult.Text += m[i] + " 元 x " + r[ i ] + "    ";
+            }
+        }
+    }
+}
